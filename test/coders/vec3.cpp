fc5761ffd2ea210e43d4f66c8c96bff45eddc0dc
JustNeed <maximus663232@gmail.com>
2026-02-14 16:19:17 +0700

    fix u8path in test/vec3.cpp

--- test/coders/vec3.cpp
+++ test/coders/vec3.cpp
@@ -7,7 +7,7 @@
 namespace fs = std::filesystem;
 
 TEST(VEC3, Decode) {
-    io::set_device("res", std::make_shared<io::StdfsDevice>(fs::u8path("../../res")));
+    io::set_device("res", std::make_shared<io::StdfsDevice>(fs::path("../../res")));
     io::path file = "res:models/block.vec3";
     auto bytes = io::read_bytes_buffer(file);
     auto model = vec3::load(file.string(), bytes);
